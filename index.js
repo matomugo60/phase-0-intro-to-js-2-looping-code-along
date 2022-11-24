15967daf2baa3281299f4cf4ca252ee1b32379a6
matomugo60 <matomugo60@gmail.com>
2022-11-24 16:35:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Code your solutions in this file
 for (let age = 30; age < 40; age++) {
     console.log(`I'm ${age} years old. Happy birthday to me!`);
-    
   }
 
   const gifts = ["teddy bear", "drone", "doll"];
 
 function wrapGifts(gifts) {
   for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    
+    console.log(`Wrapped ${gifts[i]} and added a bow!`); 
   }
 
   return gifts;
